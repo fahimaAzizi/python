70260f0218c8cf318f984d01fbd33c062b717584
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2024-03-21 00:28:16 +0500

    Update python2.py

--- python1/python2.py
+++ python1/python2.py
@@ -1,11 +1,25 @@
-text = 'Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
+text = 'Hello Zaira'
+custom_key = 'python'
 
-for char in text.lower():
-    print(char == ' ')
-    index = alphabet.find(char)
-    new_index = index + shift
-    encrypted_text += alphabet[new_index]
-    print('char:', char, 'encrypted text:', encrypted_text)
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+    
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    
+    return encrypted_text
